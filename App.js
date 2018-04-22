56780186890728d94e6f96d4f2eafbaf2d831c8d
Taro Matsuzawa <taro@georepublic.co.jp>
2018-04-22 10:06:14 +0900

    ステータスバーの高さを調整

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform  } from 'react-native';
+
+const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
 export default class App extends React.Component {
   render() {
@@ -15,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: STATUSBAR_HEIGHT,
   },
 });
