c5b8a7352a66200d055a91c6f50a68cc9323dc6e
Taro Matsuzawa <taro@georepublic.co.jp>
2018-05-03 14:42:10 +0900

    コメントを追加

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Platform  } from 'react-native';
+// 1: StatusBarとPlatformをimport対象に追加
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
 
+// 2: 高さの判断をして値を設定
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
 export default class App extends React.Component {
@@ -17,6 +19,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // 3 : paddingTop にステータスバーの高さを指定して下にずらす
     paddingTop: STATUSBAR_HEIGHT,
+    // 4 : 使わないスタイルを削除する
+//    alignItems: 'center',
+//    justifyContent: 'center',
   },
 });
