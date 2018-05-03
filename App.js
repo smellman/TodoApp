6988d64eda556092fc93375afcef057924147880
Taro Matsuzawa <taro@georepublic.co.jp>
2018-05-03 14:45:31 +0900

    FlatListを実装, コメントを追加

--- App.js
+++ App.js
@@ -5,24 +5,40 @@ import {
   View,
   StatusBar,
   Platform,
-  ScrollView, // 1: スクロールビューのインポート
+  ScrollView,
+  FlatList, // 1: FlatListを追加
 } from 'react-native';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
 export default class App extends React.Component {
+
+  // 2: コンストラクタを定義
+  constructor(props) {
+    super(props)
+    // 3: state を初期化
+    this.state = {
+      todo: [
+        {index: 1, title: "原稿を書く", done: false},
+        {index: 2, title: "犬の散歩をする", done: false}
+      ],
+      currentIndex: 2
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        { /* 2: フィルタの部分 */ }
         <View style={styles.filter}>
           <Text>Filterがここに配置されます</Text>
         </View>
-        { /* 3: TODOリスト */ }
         <ScrollView style={styles.todolist}>
-          <Text>Todoリストがここに配置されます</Text>
+          { /* 4: FlatListを実装 */ }
+          <FlatList data={this.state.todo}
+            renderItem={({item}) => <Text>{item.title}</Text>}
+            keyExtractor={(item, index) => "todo_" + item.index}
+          />
         </ScrollView>
-        { /* 4: 入力スペース */ }
         <View style={styles.input}>
           <Text>テキスト入力がここに配置されます</Text>
         </View>
@@ -37,7 +53,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop: STATUSBAR_HEIGHT,
   },
-  // 5: 追加したUIのスタイル
   filter: {
     height: 30,
   },
